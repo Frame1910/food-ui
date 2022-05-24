da4359e7c94d769f481b67c50d7493c83bf343a4
Darren <dpmeiring@gmail.com>
2022-05-24 14:42:52 +0800

    Basic search

--- src/app/search-bar/search-bar.component.ts
+++ src/app/search-bar/search-bar.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { FormControl } from '@angular/forms';
+import { Router } from '@angular/router';
+import { debounceTime } from 'rxjs';
+import { ApiService, RedisDocument, RedisResponse } from '../service/api.service';
 
 @Component({
   selector: 'app-search-bar',
@@ -9,9 +12,25 @@ import { FormControl } from '@angular/forms';
 export class SearchBarComponent implements OnInit {
   searchInput = new FormControl('');
 
-  constructor() { }
+  searchResults: RedisResponse | undefined;
+
+  constructor(
+    private apiService: ApiService,
+    private router: Router,
+  ) { }
 
   ngOnInit(): void {
+    this.searchInput.valueChanges.pipe(
+      debounceTime(300),
+    ).subscribe(term => {
+      this.apiService.queryRedis(term).subscribe(results => {
+        this.searchResults = results;
+      })
+    });
+  }
+
+  route(data: RedisDocument) {
+    console.log(data)
   }
 
 }
